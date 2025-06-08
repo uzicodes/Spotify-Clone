2830c0e2e71818cff5a4496fef44820f65926765
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-06-08 17:32:43 +0600

    added files

---
+++ spotify-x/src/login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './login'
+function Login() {
+  return (
+    
+    
+    <div className="login">
+      <h1> Iam the login page</h1>
+      <img src="" alt=""/>
+       {/* Spotify logo*/}
+       {/* Login with Spotify button */}
+    </div>
+
+  )
+}
+
+
+
+export default Login;
